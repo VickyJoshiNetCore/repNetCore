1b32d502b00e03ad34525306b93cac5d17175ebc
VickyJoshiNetCore <itsmevickeyjoshi@gmail.com>
2021-04-02 19:13:38 +0530

    new

--- WebGentleBookStore/WebGentleBookStore/Startup.cs
+++ WebGentleBookStore/WebGentleBookStore/Startup.cs
@@ -32,7 +32,7 @@ namespace WebGentleBookStore
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello World!!");
                 });
             });
         }
